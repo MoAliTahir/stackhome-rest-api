9f8898f6a229b0a57bfff7d2b12180e99a65ae57
AliTahir <mo.alitahir9@gmail.com>
2019-09-13 18:10:40 +0100

    Using GENERIC class-based views

--- stackhome/views.py
+++ stackhome/views.py
@@ -1,52 +1,13 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
 from stackhome.models import Apartment
 from stackhome.serializers import ApartmentSerializer
 
 
-class ApartmentList(APIView):
-
-    def get(self, request, format=None):
-        """
-        List all code snippets, or create a new snippet.
-        """
-        apartment = Apartment.objects.all()
-        serializer = ApartmentSerializer(apartment, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ApartmentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ApartmentList(generics.ListCreateAPIView):
+    queryset = Apartment.objects.all()
+    serializer_class = ApartmentSerializer
 
 
-class ApartmentDetail(APIView):
-    """
-            Retrieve, update or delete a code snippet.
-            """
-    def get_object(self, request, pk):
-        try:
-            return Apartment.objects.get(pk=pk)
-        except Apartment.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        serializer = ApartmentSerializer(self.get_object(request, pk))
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        apartment = self.get_object(request, pk)
-        serializer = ApartmentSerializer(apartment, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        apartment = self.get_object(request, pk)
-        apartment.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ApartmentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Apartment.objects.all()
+    serializer_class = ApartmentSerializer
